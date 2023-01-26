5f440cf7385e73b48abd3883645e709164e326fc
tulay <tmagar448@gmail.com>
2023-01-25 19:58:31 -0500

    Add project files.

---
+++ BlogApplication/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BlogApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApplication.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+    }
+}
---
+++ BlogApplication/Models/BlogPost.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogApplication.Models
+{
+    public class BlogPost
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Date Created")]
+        public DateTime DateCreated { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Date Modified")]
+        public DateTime DateModified { get; set; }
+
+        public string Author { get; set; }
+        
+        
+    }
+}
